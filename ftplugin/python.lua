5ae600593ab85b7c93f24544dd7210d2afc0f1a0
xssaw <2815657324@qq.com>
2022-03-01 00:24:50 +0800

    更新配置

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,7 @@
-vim.bo.expandtab = true                     
+vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
-vim.bo.tabstop = 4                     
-vim.bo.softtabstop = 4 
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:⏎")
 
--- lua/conf/auto-session.lua
+++ lua/conf/auto-session.lua
@@ -6,8 +6,7 @@ require("auto-session").setup(
         -- 自动加载最后保存的一次会话
         auto_session_enable_last_session = true,
         -- 保存会话时自动关闭 nvim-tree
-        -- 这是因为 nvim-tree 如果处于开启
-        -- 状态，会破坏会话的保存
+        -- 这是因为 nvim-tree 如果处于开启状态，会破坏会话的保存
         pre_save_cmds = {"tabdo NvimTreeClose"}
     }
 )
