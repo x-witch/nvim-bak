900f091a52f6fa161f8b8bd46fda7c5ffccb67ac
xssaw <2815657324@qq.com>
2022-03-10 20:33:47 +0800

    更新配置

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -2,8 +2,9 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:⏎")
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:⏎")
+-- vim.opt.listchars:append("eol:↴")
 
 -- 一键运行python
 vim.cmd(
