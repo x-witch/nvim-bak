c3e695cc5b1b04510ac7d4ff0d067279dfed3ce0
xssaw <2815657324@qq.com>
2022-04-29 21:15:58 +0800

    更新配置

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -1,36 +1,116 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
-require("nvim-tree").setup(
-    {
-        -- 自动关闭
-        auto_close = false,
-        -- 视图
-        view = {
-            -- 宽度
-            width = 30,
-            -- 高度
-            height = 30,
-            -- 隐藏顶部的根目录显示
-            hide_root_folder = false,
-            -- 自动调整大小
-            auto_resize = true
+require("nvim-tree").setup({
+    auto_reload_on_write = true,
+    disable_netrw = false,
+    hijack_cursor = false,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    update_cwd = false,
+    view = {
+        width = 30,
+        height = 30,
+        hide_root_folder = false,
+        side = "left",
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+            custom_only = false,
+            list = {
+                -- user mappings go here
+            },
         },
-        diagnostics = {
-            -- 是否启用文件诊断信息
+    },
+    renderer = {
+        indent_markers = {
             enable = false,
             icons = {
-                hint = "",
-                info = "",
-                warning = "",
-                error = ""
-            }
+                corner = "└ ",
+                edge = "│ ",
+                none = "  ",
+            },
         },
-        git = {
-            -- 是否启用 git 信息
+        icons = {
+            webdev_colors = true,
+        },
+    },
+    hijack_directories = {
+        enable = true,
+        auto_open = true,
+    },
+    update_focused_file = {
+        enable = false,
+        update_cwd = false,
+        ignore_list = {},
+    },
+    ignore_ft_on_setup = {},
+    system_open = {
+        cmd = "",
+        args = {},
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    filters = {
+        dotfiles = false,
+        custom = {},
+        exclude = {},
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 400,
+    },
+    actions = {
+        use_system_clipboard = true,
+        change_dir = {
             enable = true,
-            ignore = true,
-            timeout = 500
-        }
-    }
+            global = false,
+            restrict_above_cwd = false,
+        },
+        open_file = {
+            quit_on_open = false,
+            resize_window = false,
+            window_picker = {
+                enable = true,
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                exclude = {
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                },
+            },
+        },
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    log = {
+        enable = false,
+        truncate = false,
+        types = {
+            all = false,
+            config = false,
+            copy_paste = false,
+            diagnostics = false,
+            git = false,
+            profile = false,
+        },
+    },
+}
 )
 -- 默认图标，可自行修改
 vim.g.nvim_tree_icons = {
@@ -58,6 +138,18 @@ vim.g.nvim_tree_icons = {
         symlink_open = ""
     }
 }
+
+-- with relative path
+require "nvim-tree.events".on_file_created(function(file) vim.cmd("edit " .. file.fname) end)
+-- with absolute path
+-- require"nvim-tree.events".on_file_created(function(file) vim.cmd("edit "..vim.fn.fnamemodify(file.fname, ":p")) end)
+
+-- auto close feature
+vim.cmd(
+  [[
+    autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+]]
+)
 -- 目录后加上反斜杠 /
 vim.g.nvim_tree_add_trailing = 1
 -- 按 leader 1 打开文件树
