60c2e26f033aba42ff1fcab1a2993a50930a0f1f
xssaw <2815657324@qq.com>
2022-05-02 00:16:29 +0800

    更新配置

--- lua/conf/indent-blankline.lua
+++ lua/conf/indent-blankline.lua
@@ -1,53 +1,95 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
-require("indent_blankline").setup(
-    {
-        -- 显示当前所在区域
-        show_current_context = true,
-        -- 显示当前所在区域的开始位置
-        show_current_context_start = true,
-        -- 显示行尾符
-        show_end_of_line = true
-    }
-)
-vim.g.indent_blankline_show_first_indent_level = true
+local status_ok, indent_blankline = pcall(require, "indent_blankline")
+if not status_ok then
+	return
+end
+
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
-  -- 'startify',
-  "dashboard",
-  -- 'dotooagenda',
-  -- 'log',
-  -- 'fugitive',
-  -- 'gitcommit',
-  -- 'packer',
-  -- 'vimwiki',
-  -- 'markdown',
-  -- 'json',
-  -- 'txt',
-  -- 'vista',
-  -- 'help',
-  -- 'todoist',
-  -- 'NvimTree',
-  -- 'peekaboo',
-  -- 'git',
-  -- 'TelescopePrompt',
-  -- 'undotree',
-  -- 'flutterToolsOutline',
-  "" -- for all buffers without a file type
+	"help",
+	"startify",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+    -- 'dotooagenda',
+    -- 'log',
+    -- 'fugitive',
+    -- 'gitcommit',
+    -- 'packer',
+    -- 'vimwiki',
+    -- 'markdown',
+    -- 'json',
+    -- 'txt',
+    -- 'vista',
+    -- 'help',
+    -- 'todoist',
+    -- 'NvimTree',
+    -- 'peekaboo',
+    -- 'git',
+    -- 'TelescopePrompt',
+    -- 'undotree',
+    -- 'flutterToolsOutline',
+    "" -- for all buffers without a file type
 }
-vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
+vim.g.indentLine_enabled = 1
+-- vim.g.indent_blankline_char = "│"
+vim.g.indent_blankline_char = "▏"
+-- vim.g.indent_blankline_char = "▎"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {
-  "class",
-  "function",
-  "method",
-  "block",
-  "list_literal",
-  "selector",
-  "^if",
-  "^table",
-  "if_statement",
-  "while",
-  "for"
+	"class",
+	"return",
+	"function",
+	"method",
+	"^if",
+	"^while",
+	"jsx_element",
+	"^for",
+	"^object",
+	"^table",
+	"block",
+	"arguments",
+	"if_statement",
+	"else_clause",
+	"jsx_element",
+	"jsx_self_closing_element",
+	"try_statement",
+	"catch_clause",
+	"import_statement",
+	"operation_type",
 }
+-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+vim.wo.colorcolumn = "99999"
+
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+-- vim.opt.list = true
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "space:"
+-- vim.opt.listchars:append "eol:↴"
+
+indent_blankline.setup({
+    -- 显示行尾符
+	-- show_end_of_line = true,
+	-- space_char_blankline = " ",
+    -- 显示当前所在区域
+	show_current_context = true,
+    -- 显示当前所在区域的开始位置
+	-- show_current_context_start = true,
+	-- char_highlight_list = {
+	--   "IndentBlanklineIndent1",
+	--   "IndentBlanklineIndent2",
+	--   "IndentBlanklineIndent3",
+	-- },
+})
 -- because lazy load indent-blankline so need readd this autocmd
 vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
