a22038cbdc8c93fba611f8150e7d8e29b171541a
xssaw <2815657324@qq.com>
2022-05-07 21:13:35 +0800

    更新配置

--- init.lua
+++ init.lua
@@ -4,5 +4,6 @@ require("basic.keymaps")
 require("basic.plugins")
 require("basic.custom")
 require('conf')
+require('utils.colorscheme')
 -- 加载缓存优化 ./cache/nvim/luacache_chunks文件
 require('impatient')
---
+++ lua/utils/colorscheme.lua
@@ -0,0 +1,15 @@
+-- cursor color: #61AFEF
+-- local colorscheme = "catppuccin"
+local colorscheme = "github_light"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+if colorscheme == "catppuccin" then
+  require "themes.catppuccin"
+elseif colorscheme == "github_light" then
+  require "themes.github-nvim-theme"
+end
