1648fe50564bf3b1457dbd4a7974b50545837e8b
xssaw <2815657324@qq.com>
2022-05-19 23:44:52 +0800

    更新配置

--- lua/conf/nvim-hlslens.lua
+++ lua/conf/nvim-hlslens.lua
@@ -1,6 +1,5 @@
 -- https://github.com/kevinhwang91/nvim-hlslens
 -- 其实不用管下面这些键位绑定是什么意思，总之按下这些键位后会出现当前搜索结果的条目数量
--- https://github.com/kevinhwang91/nvim-hlslens
 
 local status_ok, hlslens = pcall(require, "hlslens")
 if not status_ok then
@@ -14,3 +13,4 @@ hlslens.setup({
   nearest_float_when = 'always',
 })
 require("scrollbar.handlers.search").setup()
+
